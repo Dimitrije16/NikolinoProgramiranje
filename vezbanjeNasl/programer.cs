6707e48010cb162b0b51e57885f5854dfef58bcf
Dimitrije Maravić <dmaravic@smart.edu.rs>
2022-01-20 17:46:43 +0100

    Pocetni komit

---
+++ vezbanjeNasl/programer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vezbanjeNasl
+{
+    class programer : radnik
+    {
+        public double bonus;
+        public programer(string ime, string prezime,
+            string jmbg, double zarada, double bonus)
+            : base(ime, prezime, jmbg, zarada)
+        {
+            this.bonus = bonus;
+        }
+
+        public override double godisnjaPlata()
+        {
+            return base.godisnjaPlata() + bonus / 100 * base.godisnjaPlata();
+        }
+    }
+}
---
+++ vezbanjeNasl/radnik.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vezbanjeNasl
+{
+    class radnik
+    {
+        public string ime;
+        public string prezime;
+        public string jmbg;
+
+        public double zarada;
+        public radnik(string ime, string prezime, string jmbg, double zarada)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.jmbg = jmbg;
+            this.zarada = zarada;
+        }
+        public virtual double godisnjaPlata()
+        {
+            return zarada * 12;
+        }
+    }
+}
